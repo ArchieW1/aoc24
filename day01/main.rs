1b7e9f372637ba158e4e50ae9383c9bdd61ab486
ArchieW1 <archie.williamson1@gmail.com>
2024-12-01 01:05:01 +0000

    initial commit

---
+++ day01/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let input = include_str!("input.txt");
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> &str {""}
+
+fn part2(input: &str) -> &str {""}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_part1() {
+        assert_eq!("1", "1");
+    }
+}
